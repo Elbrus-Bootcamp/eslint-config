6ed2f45326842eefcd07996fdc9212b2d4c02147
grentank <mexmat100394@gmail.com>
2024-06-06 12:02:12 +0300

    added ignored rules

--- rules/ignored.js
+++ rules/ignored.js
@@ -1,3 +1,12 @@
 export default {
-  rules: {},
+  rules: {
+    // Do not need the strict mode
+    'strict': 'off',
+
+    // Flux reducers violate this rule
+    'default-param-last': 'warn',
+
+    // Prefer ++ over += 1
+    'no-plusplus': 'off',
+  },
 };
